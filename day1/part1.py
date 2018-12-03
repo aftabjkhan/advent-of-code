9c34d014968c8d256a38f947951c211714f96bd9
Aftab Khan <aftabjkhan@gmail.com>
2018-12-02 23:21:22 -0500

    Main and test methods for day1 part1, method to calculate frequency deltas

--- day1/part1.py
+++ day1/part1.py
@@ -1,7 +1,46 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import sys
 
-if len(sys.argv != 2):
-	print("Usage: part1.py input_file_path")
-	exit(1)
 
+"""
+Challenge:
+https://adventofcode.com/2018/day/1
+"""
+
+
+def calculate_final_freqnecy(frequency_strings_list):
+  frequency_deltas = [ int(x) for x in frequency_strings_list ]
+
+  #initial frequency = 0
+  frequency = 0;
+
+  #apply all deltas to the frequency
+  for delta in frequency_deltas:
+    frequency = frequency + delta
+
+  return frequency;
+
+
+
+def test(case):
+
+  #tests input is single line, comma separated. Convert to list
+  test_strings = case.split(",")
+
+  return calculate_final_freqnecy(test_strings);
+
+
+
+if __name__ == '__main__':
+  
+  #check arg count
+  if len(sys.argv) != 2:
+    print("Usage: part1.py input_file_path")
+    exit(1)
+
+  #get frequency strings list
+  input_file_path = sys.argv[1]
+  file = open(input_file_path, "r")
+  lines = [line.rstrip('\n') for line in file]
+
+  print(calculate_final_freqnecy(lines))
