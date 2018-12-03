b1ef4c5ff4b1bf7f69ee8c3d9c7dd36df0dc8aa4
Aftab Khan <aftabjkhan@gmail.com>
2018-12-03 01:44:39 -0500

    Solution for d2p1

--- day2/part1.py
+++ day2/part1.py
@@ -8,21 +8,55 @@ https://adventofcode.com/2018/day/2
 """
 
 
-def calculate_final_frequency(frequency_strings_list):
-  frequency_deltas = [ int(x) for x in frequency_strings_list ]
+def get_two_three_char_count(box_id):
 
-  #initial frequency = 0
-  frequency = 0;
+  box_chars = list(box_id)
 
-  #apply all deltas to the frequency
-  for delta in frequency_deltas:
-    frequency = frequency + delta
+  char_frequencies = {}
 
-  return frequency;
+  # for each char in the box id, increment frequency in the char map
+  for c in box_chars:
+    if c in char_frequencies:
+      char_frequencies[c] += 1
+    else:
+      char_frequencies[c] = 1
 
+  two_instances = 0
+  three_instances = 0
+
+  # for each char that occurs 2 or 3 times, increment 2 & 3 instance counters
+  for value in char_frequencies.values():
+    if value == 2:
+      two_instances += 1
+    if value == 3:
+      three_instances += 1
+
+  # return number of chars that occur 2 and 3 times as a tuple
+  return (two_instances, three_instances);
+
+
+
+def calculate_boxes_checksum(box_id_list):
+  
+  boxes_with_two_instances = 0
+  boxes_with_three_instances = 0
+
+  for box_id in box_id_list:
+    char_count = get_two_three_char_count(box_id)
+
+    if char_count[0] > 0:
+      boxes_with_two_instances += 1
+    if char_count[1] > 0:
+      boxes_with_three_instances += 1
+
+  return (boxes_with_two_instances * boxes_with_three_instances)
 
 
 def test(case):
+  #tests input is single line, comma separated. Convert to list
+  test_strings = case.split(",")
+
+  return calculate_boxes_checksum(test_strings);
 
 
 
@@ -37,3 +71,5 @@ if __name__ == '__main__':
   input_file_path = sys.argv[1]
   file = open(input_file_path, "r")
   lines = [line.rstrip('\n') for line in file]
+
+  print(calculate_boxes_checksum(lines))
